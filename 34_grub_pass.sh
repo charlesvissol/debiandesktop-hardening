f43683a958727143ba26ea9a7758acfb424a4f01
Antoine Pulon <83464334+MagicPupu@users.noreply.github.com>
2023-04-24 14:59:24 +0200

    Adding banners to Antoine scripts

--- 34_grub_pass.sh
+++ 34_grub_pass.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Copyright 2023 Angrybee.tech (https://angrybee.tech)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+echo "#############################################################"
+echo "#   Step 34: Setting up a passwphrase for grub init - START #"
+echo "#############################################################"
+
 # root privileges
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
@@ -26,3 +45,7 @@ if [ "$password" == "$password_confirmation" ]; then
 else
     echo "Passwords do not match. Please try again."
 fi    
+
+echo "#############################################################"s
+echo "#   Step 34: Setting up a passwphrase for grub init - END   #"
+echo "#############################################################"
