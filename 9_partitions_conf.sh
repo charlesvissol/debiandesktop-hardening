f5af927a778797ba6c9cf7be1884dc4ab9adf12c
Antoine Pulon <83464334+MagicPupu@users.noreply.github.com>
2023-06-30 14:01:43 +0200

    update nvme

--- 9_partitions_conf.sh
+++ 9_partitions_conf.sh
@@ -26,20 +26,20 @@ myhost=$(cat /proc/sys/kernel/hostname)
 echo "#Step 9: Configure Partitions --> MSG: backup /etc/fstab to current directory"
 cp /etc/fstab ./fstab.save
 
-echo "#Step 9: Configure Partitions --> MSG: update /boot partitions"
-numline1=$(cat -n /etc/fstab | grep 'UUID' | grep '/boot           ext2' | awk '{print $1;}')
-sed -i ${numline1}'s/defaults/nosuid,nodev,noexec/' /etc/fstab
+#echo "#Step 9: Configure Partitions --> MSG: update /boot partitions"
+#numline1=$(cat -n /etc/fstab | grep 'UUID' | grep '/boot           ext2' | awk '{print $1;}')
+#sed -i ${numline1}'s/defaults/nosuid,nodev,noexec/' /etc/fstab
 
 echo "#Step 9: Configure Partitions --> MSG: update /home partitions"
-numline2=$(cat -n /etc/fstab | grep '/dev/mapper/'${myhost}'--vg-home /home' | awk '{print $1;}')
+numline2=$(cat -n /etc/fstab | grep '/home           ext4' | awk '{print $1;}')
 sed -i ${numline2}'s/defaults/nodev/' /etc/fstab
 
 echo "#Step 9: Configure Partitions --> MSG: update /tmp partitions"
-numline3=$(cat -n /etc/fstab | grep '/dev/mapper/'${myhost}'--vg-tmp /tmp' | awk '{print $1;}')
+numline3=$(cat -n /etc/fstab | grep '/tmp            ext4' | awk '{print $1;}')
 sed -i ${numline3}'s/defaults/nosuid,nodev,noexec/' /etc/fstab
 
 echo "#Step 9: Configure Partitions --> MSG: update /var partitions"
-numline4=$(cat -n /etc/fstab | grep '/dev/mapper/'${myhost}'--vg-var /var' | awk '{print $1;}')
+numline4=$(cat -n /etc/fstab | grep '/var            ext4' | awk '{print $1;}')
 sed -i ${numline4}'s/defaults/nosuid,nodev/' /etc/fstab
 
 echo "#Step 9: Configure Partitions --> MSG: add tmpfs at the end of /etc/fstab"
